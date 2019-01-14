3cfbe4613bdac68f9aae42c52f4ed034ac2e6392
hbina <hbina4326>
2019-01-14 16:42:05 -0500

    factorized the checks between if itemType exists or if Key exists..

--- app/src/main/java/com/akarin/hbina/akarinspending/util/AkarinDatabase.java
+++ app/src/main/java/com/akarin/hbina/akarinspending/util/AkarinDatabase.java
@@ -8,61 +8,67 @@ import com.github.mikephil.charting.data.PieEntry;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
+/**
+ * Container to valid instances of AkarinItem.
+ */
 public class AkarinDatabase {
 
-  private static final Long UNIX_MONTH = 2592000L;
-  private static final String TAG = "AkarinDatabase";
-  private static AkarinDatabase akarinDatabase;
-  private HashMap<String, AkarinItem> itemHash = new HashMap<>();
-  private HashMap<String, Integer> itemIndexHash = new HashMap<>();
-  private ArrayList<PieEntry> alpe = new ArrayList<>();
-  private int counter = 0;
+    private static final Long UNIX_MONTH = 2592000L;
+    private static final String TAG = "AkarinDatabase";
+    private static AkarinDatabase akarinDatabase;
+    private HashMap<String, AkarinItem> hashOfStringToAkarinItem = new HashMap<>();
+    private HashMap<String, Integer> hashOfStringToInteger = new HashMap<>();
+    private ArrayList<PieEntry> arrayOfPieEntries = new ArrayList<>();
+    private int counter = 0;
 
-  private AkarinDatabase() {
-  }
-
-  public static AkarinDatabase getDatabase() {
-    if (akarinDatabase == null) {
-      akarinDatabase = new AkarinDatabase();
+    private AkarinDatabase() {
     }
-    return akarinDatabase;
-  }
 
-  public void addItem(String key, AkarinItem item) {
-    if (!itemIndexHash.containsKey(item.getItemType())) {
-      itemIndexHash.put(item.getItemType(), counter);
-      alpe.add(new PieEntry(0, item.getItemType()));
-      counter++;
+    public static AkarinDatabase getDatabase() {
+        if (akarinDatabase == null) {
+            akarinDatabase = new AkarinDatabase();
+        }
+        return akarinDatabase;
     }
-    if (!itemHash.containsKey(key)) {
-      Log.d(TAG, item.toString());
-      addPriceToItemIndex(item.getItemType(), item.getItemPrice());
-      itemHash.put(key, item);
+
+    public void addItem(String key, AkarinItem item) {
+        addItemType(item.getItemType());
+        if (!hashOfStringToAkarinItem.containsKey(key)) {
+            Log.d(TAG, item.toString());
+            addPriceToItemIndex(item.getItemType(), item.getItemPrice());
+            hashOfStringToAkarinItem.put(key, item);
+        }
     }
-  }
 
-  public ArrayList<AkarinItem> getAllItem() {
-    pruneDatabase(System.currentTimeMillis());
-    return new ArrayList<>(itemHash.values());
-  }
-
-  private void pruneDatabase(Long currentTime) {
-    for (String s : itemHash.keySet()) {
-      if (itemHash.get(s).getItemTime() < (currentTime - UNIX_MONTH)) {
-        addPriceToItemIndex(itemHash.get(s).getItemType(), -itemHash.get(s).getItemPrice());
-        itemHash.remove(s);
-      }
+    private void addItemType(String itemType) {
+        if (!hashOfStringToInteger.containsKey(itemType)) {
+            hashOfStringToInteger.put(itemType, counter);
+            arrayOfPieEntries.add(new PieEntry(0, itemType));
+            counter++;
+        }
     }
-  }
 
-  private void addPriceToItemIndex(String itemType, Float price) {
-    int indexOfItemType = itemIndexHash.get(itemType);
-    Float itemTypePriceSum = price + alpe.get(indexOfItemType).getValue();
-    alpe.set(indexOfItemType, new PieEntry(itemTypePriceSum, itemType));
-  }
+    public ArrayList<AkarinItem> getAllItem() {
+        pruneDatabase(System.currentTimeMillis());
+        return new ArrayList<>(hashOfStringToAkarinItem.values());
+    }
 
-  public ArrayList<PieEntry> getAllItemAsPieEntries() {
-    return alpe;
-  }
+    private void pruneDatabase(Long currentTime) {
+        for (String s : hashOfStringToAkarinItem.keySet()) {
+            if (hashOfStringToAkarinItem.get(s).getItemTime() < (currentTime - UNIX_MONTH)) {
+                addPriceToItemIndex(hashOfStringToAkarinItem.get(s).getItemType(), -hashOfStringToAkarinItem.get(s).getItemPrice());
+                hashOfStringToAkarinItem.remove(s);
+            }
+        }
+    }
+
+    private void addPriceToItemIndex(String itemType, Float price) {
+        int indexOfItemType = hashOfStringToInteger.get(itemType);
+        Float itemTypePriceSum = price + arrayOfPieEntries.get(indexOfItemType).getValue();
+        arrayOfPieEntries.set(indexOfItemType, new PieEntry(itemTypePriceSum, itemType));
+    }
+
+    public ArrayList<PieEntry> getAllItemAsPieEntries() {
+        return arrayOfPieEntries;
+    }
 }
